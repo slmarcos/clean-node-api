fc8dff668b911d397dd23169fd0cb79e5da6a7de
Marcos Lopes <sl.marcos15@hotmail.com>
2020-12-23 21:49:46 -0300

    test: clean account collection before each test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -12,6 +12,11 @@ describe('SignUp Routes', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
