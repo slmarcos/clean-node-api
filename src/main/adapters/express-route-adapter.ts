1373abbec9e6aff25fc5ed0ade7feeaa2841af71
Marcos Lopes <sl.marcos15@hotmail.com>
2021-01-10 03:12:55 -0300

    feat: ensure save survey results returns 200 on success

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -4,7 +4,9 @@ import { Request, Response } from 'express'
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
-      body: req.body
+      params: req.params,
+      body: req.body,
+      accountId: req.accountId
     }
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
