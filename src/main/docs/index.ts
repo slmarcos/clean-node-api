d184b6c7b5f424c439c8a17998121dd623e1445e
Marcos Lopes <sl.marcos15@hotmail.com>
2021-01-11 01:40:15 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Documentação API curso NodeJs, Typescript, TDD, Clean Architecture e SOLID - Rodrigo Manguinho - para realização de enquetes entre programadores',
+    version: '1.0.0'
+  }
+}
