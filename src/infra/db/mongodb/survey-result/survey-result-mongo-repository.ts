994f1836be35f258b899113b11e2c85ab4a94ade
Marcos Lopes <sl.marcos15@hotmail.com>
2021-01-14 01:40:42 -0300

    test: ensure SurveyResultMongoRepository returns null on load survey without answers

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -176,6 +176,6 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository, 
       })
       .build()
     const surveyResult = await surveyResultCollection.aggregate(query).toArray()
-    return surveyResult?.length ? surveyResult[0] : null as unknown as SurveyResultModel
+    return surveyResult.length ? surveyResult[0] : null as unknown as SurveyResultModel
   }
 }
